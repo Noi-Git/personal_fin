047d86a813bfe00c8965911c15520067b4657aff
Noi <noi.patsin@gmail.com>
2019-10-30 18:43:45 -0700

    setup React to display output data from database

---
+++ client/src/components/pages/landing.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './landing.css';
+
+class Landing extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('/daily')
+      .then(res => res.json())
+      .then(data =>
+        this.setState({ data }, () => console.log('money detail....', data))
+      );
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Landing</h2>
+        <div>
+          {this.state.data.map(dailyData => (
+            <ul key={dailyData.id}>
+              <li>Name: {dailyData.name}</li>
+              <li>Amount: {dailyData.amount}</li>
+              <li>For: {dailyData.totaldays} days</li>
+              <li>Summerry/day: ${dailyData.perday}</li>
+            </ul>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
