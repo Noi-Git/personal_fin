869002132bca642dbf2956ae3e2330896a63636f
Noi <noi.patsin@gmail.com>
2019-11-14 10:08:57 -0800

    create get endpoint and query to get sum numbers from multiple table

--- routes/api/info.js
+++ routes/api/info.js
@@ -6,31 +6,34 @@ const pool = require('../../db/index');
 router.get('/total', async (request, response) => {
   try {
     const total_q = `
-    SELECT inco.user_id,inco2.total_income,  expe.total_expense, res.total_reserve
+    SELECT distinct inco.user_id,inco2.total_income,  expe.total_expense, res.total_reserve
     FROM incomes AS inco
-    LEFT JOIN
+	
+    INNER JOIN
     (SELECT user_id, SUM(i_amount) AS total_income 
     FROM incomes 
-    WHERE user_id = 1
+	  where user_id = 1
     GROUP BY user_id) AS inco2 
     ON inco.user_id = inco2.user_id
-    LEFT JOIN
+	
+    INNER JOIN
     (SELECT user_id, SUM(e_amount) AS total_expense
     FROM expenses 
-    WHERE user_id = 1
+	  where user_id = 1
     GROUP BY user_id) AS expe 
     ON expe.user_id = inco2.user_id
-    LEFT JOIN
+	
+    INNER JOIN
     (SELECT user_id, SUM(r_amount) AS total_reserve
     FROM reserve_fund 
-    WHERE user_id = 1
+	  where user_id = 1
     GROUP BY user_id) AS res 
     ON res.user_id = inco2.user_id`;
 
     const total_result = await pool.query(total_q); // return from query
     // console.log(total_result.rows);
 
-    response.json(total_result);
+    response.json(total_result.rows[0]);
 
     if (!total_result) {
       //return res.status(400).json({ msg: 'There is no money infomation' });
