d308a8e7dcea25174987727fdd5e8dd18d754404
Noi <noi.patsin@gmail.com>
2019-11-15 11:32:38 -0800

    refactor select query into a function selectIncomeFromDatabase which it can be reuse in put endpoint

--- routes/api/income.js
+++ routes/api/income.js
@@ -3,25 +3,38 @@ const router = express.Router();
 
 const pool = require('../../db/index');
 
+const selectIncomeFromDatabase = async () => {
+  const income_q = `
+  SELECT DISTINCT inco.*, total_income
+  FROM incomes AS inco
+  INNER JOIN (SELECT user_id, SUM(i_amount) AS total_income
+     FROM incomes
+     GROUP BY user_id) AS inco_total
+     ON inco.user_id = inco_total.user_id
+  ORDER BY inco.user_id`;
+
+  return await pool.query(income_q); // return from query
+};
+
 /* ===== GET INCOME ===== */
 // @route   GET routes/api/income
 // @desc    Get user income infomation
 // @access  Private
 router.get('/income', async (req, res) => {
   try {
-    const income_q = `
-    SELECT DISTINCT inco.*, total_income
-    FROM incomes AS inco
-    INNER JOIN (SELECT user_id, SUM(i_amount) AS total_income
-		   FROM incomes
-		   GROUP BY user_id) AS inco_total
-		   ON inco.user_id = inco_total.user_id
-		
-    ORDER BY inco.user_id`;
+    // const income_q = `
+    // SELECT DISTINCT inco.*, total_income
+    // FROM incomes AS inco
+    // INNER JOIN (SELECT user_id, SUM(i_amount) AS total_income
+    //    FROM incomes
+    //    GROUP BY user_id) AS inco_total
+    //    ON inco.user_id = inco_total.user_id
+    // ORDER BY inco.user_id`;
 
-    const income_result = await pool.query(income_q); // return from query
+    // const income_result = await pool.query(income_q); // return from query
     // console.log(total_result.rows);
 
+    const income_result = await selectIncomeFromDatabase();
     res.json(income_result.rows);
 
     if (!income_result) {
@@ -66,15 +79,28 @@ VALUES(${user_id}, '${i_name}', ${i_amount}, '${cleared}', current_timestamp) RE
 // @route   PUT routes/api/income
 // @desc    Update user income infomation
 // @access  Private
-router.put('/income/:user_id', async (req, res) => {
-  const { user_id, id, i_name, i_amount, cleared } = req.body;
+router.put('/income', async (req, res) => {
+  const { user_id, id, i_name, i_amount } = req.body;
   try {
-    const update_income_q = `
-    UPDATE incomes SET i_name='${i_name}', i_amount='${i_amount}', cleared='${cleared}', updated_at=${NOW()}
-WHERE id=${id} AND user_id=${user_id}`;
+    //     const update_income_q = `
+    //     UPDATE incomes SET i_name='${i_name}', i_amount='${i_amount}', updated_at=${NOW()}
+    // WHERE id=${id} AND user_id=${user_id}`;
 
-    const updated_income_result = await pool.query(update_income_q); // return from query
+    // const updated_income_result = await pool.query(update_income_q); // return from query
     // console.log(total_result.rows);
+
+    // res.json(updated_income_result.rows);
+
+    const update_income_q =
+      'UPDATE incomes SET i_name=$1, i_amount=$2, updated_at=current_timestamp WHERE id=$3 AND user_id=$4';
+    const updated_income_result = await pool.query(update_income_q, [
+      i_name,
+      i_amount,
+      id,
+      user_id
+    ]);
+
+    const update_income_result = await selectIncomeFromDatabase(); // from function select * from incomes
 
     res.json(updated_income_result.rows);
 
