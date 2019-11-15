e96bd5fbd5c9a292165ad7e074192de452606d77
Noi <noi.patsin@gmail.com>
2019-11-15 14:21:08 -0800

    reverse refactor query into function to the indivdule query

--- routes/api/income.js
+++ routes/api/income.js
@@ -3,42 +3,28 @@ const router = express.Router();
 
 const pool = require('../../db/index');
 
-const selectIncomeFromDatabase = async () => {
-  const income_q = `
-  SELECT DISTINCT inco.*, total_income
-  FROM incomes AS inco
-  INNER JOIN (SELECT user_id, SUM(i_amount) AS total_income
-     FROM incomes
-     GROUP BY user_id) AS inco_total
-     ON inco.user_id = inco_total.user_id
-  ORDER BY inco.user_id`;
-
-  return await pool.query(income_q); // return from query
-};
-
 /* ===== GET INCOME ===== */
 // @route   GET routes/api/income
 // @desc    Get user income infomation
 // @access  Private
 router.get('/income', async (req, res) => {
   try {
-    // const income_q = `
-    // SELECT DISTINCT inco.*, total_income
-    // FROM incomes AS inco
-    // INNER JOIN (SELECT user_id, SUM(i_amount) AS total_income
-    //    FROM incomes
-    //    GROUP BY user_id) AS inco_total
-    //    ON inco.user_id = inco_total.user_id
-    // ORDER BY inco.user_id`;
+    const income_q = `
+    SELECT DISTINCT inco.*, total_income
+    FROM incomes AS inco
+    INNER JOIN (SELECT user_id, SUM(i_amount) AS total_income
+       FROM incomes
+       GROUP BY user_id) AS inco_total
+       ON inco.user_id = inco_total.user_id
+    ORDER BY inco.user_id`;
 
-    // const income_result = await pool.query(income_q); // return from query
+    const income_result = await pool.query(income_q); // return from query
     // console.log(total_result.rows);
-
-    const income_result = await selectIncomeFromDatabase();
-    res.json(income_result.rows);
 
     if (!income_result) {
       return res.status(400).json({ msg: 'There is no income infomation' });
+    } else {
+      res.json(income_result.rows);
     }
 
     // response.json(info);
@@ -62,10 +48,10 @@ VALUES(${user_id}, '${i_name}', ${i_amount}, '${cleared}', current_timestamp) RE
     const income_result = await pool.query(income_q); // return from query
     // console.log(total_result.rows);
 
-    res.json(income_result.rows);
-
     if (!income_result) {
       return res.status(400).json({ msg: 'Fail to insert income' });
+    } else {
+      res.json(income_result.rows);
     }
 
     // response.json(info);
@@ -91,21 +77,35 @@ router.put('/income', async (req, res) => {
 
     // res.json(updated_income_result.rows);
 
+    // update income and name of user_id: 'x' and id: 'y'
     const update_income_q =
       'UPDATE incomes SET i_name=$1, i_amount=$2, updated_at=current_timestamp WHERE id=$3 AND user_id=$4';
-    const updated_income_result = await pool.query(update_income_q, [
-      i_name,
-      i_amount,
-      id,
-      user_id
-    ]);
+    await pool.query(update_income_q, [i_name, i_amount, id, user_id]);
+    // update finish
 
-    const update_income_result = await selectIncomeFromDatabase(); // from function select * from incomes
+    // get income of user_id: 'x' and id: 'y'
+    const get_income_q = `
+    SELECT DISTINCT inco.*, total_income
+    FROM incomes AS inco
+    INNER JOIN (SELECT user_id, SUM(i_amount) AS total_income
+       FROM incomes
+       GROUP BY user_id) AS inco_total
+       ON inco.user_id = inco_total.user_id
+    WHERE inco.user_id = $1
+    AND inco.id = $2
+    ORDER BY inco.user_id`;
+    const get_income_result = await pool.query(get_income_q, [user_id, id]);
+    // done getting income
 
-    res.json(updated_income_result.rows);
-
-    if (!updated_income_result) {
-      return res.status(400).json({ msg: 'Fail to insert income' });
+    // if i dont get the income of user_id: x and id: y
+    // then something went wrong
+    if (!get_income_result) {
+      return res.status(400).json({
+        msg: `Fail to get income of user_id: ${user_id}  and id: ${id}`
+      });
+    } else {
+      // send the income data to front end
+      res.json(get_income_result.rows);
     }
 
     // response.json(info);
@@ -119,23 +119,15 @@ router.put('/income', async (req, res) => {
 // @route   DELETE routes/api/income
 // @desc    Delete user income infomation
 // @access  Private
-router.delete('/income/:user_id', async (req, res) => {
-  const { user_id, id } = req.body;
+router.delete('/income', async (req, res) => {
+  const { id } = req.body;
   try {
-    const delete_income_q = `
-    DELETE FROM incomes
-WHERE id=${id} AND user_id=${user_id}`;
+    const delete_income_q = `DELETE FROM incomes WHERE id=$1`;
 
-    const delete_income_result = await pool.query(delete_income_q); // return from query
-    // console.log(total_result.rows);
+    await pool.query(delete_income_q, [id]); // return from query
+    // console.log(delete_income_result.rows);
 
-    res.json(delete_income_result.rows);
-
-    if (!delete_income_result) {
-      return res.status(400).json({ msg: 'Fail to insert income' });
-    }
-
-    // response.json(info);
+    res.json({ msg: id + ' has been deleted' });
   } catch (err) {
     console.log(err.message);
     res.status(500).send('Server Error');
