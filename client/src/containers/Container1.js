1634f5f29986e57797b8833f8367c3099cc4d154
Noi <noi.patsin@gmail.com>
2019-11-19 15:05:35 -0800

    connect react app to redux state in container > Container1.js

--- client/src/containers/Container1.js
+++ client/src/containers/Container1.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
-import { connect } from 'react-redux'; // this is for function at the bottom
+import { connect } from 'react-redux'; // this is for function at the bottom -- connect react with redux
 
 class Container1 extends Component {
   render() {
     return (
       <div>
-        <button onClick={}> Get State </button>
-        <button onClick={}> Dispatch Action 2 </button>
+        <button onClick={console.log(this.props.stateprop1)}>Get State</button>
+        <button onClick={() => this.props.action1()}>Dispatch Action 1</button>
+        <button onClick={() => this.props.action2()}>Dispatch Action 2</button>
       </div>
     );
   }
@@ -21,9 +22,18 @@ class Container1 extends Component {
 after finish setup the functions to back to the top and import them
 */
 
-function mapStateToProps(state) {}
+function mapStateToProps(state) {
+  return {
+    stateprop1: state.stateprop1
+  };
+}
 
-function mapDispatchToProps(dispatch) {}
+function mapDispatchToProps(dispatch) {
+  return {
+    action1: () => dispatch(ACTIONS.SUCCESS),
+    action2: () => dispatch(ACTIONS.FAILURE)
+  };
+}
 
 // export default Container1;
 export default connect(mapStateToProps, mapDispatchToProps)(Container1);
